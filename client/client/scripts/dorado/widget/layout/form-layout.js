331a10a2429c8264be28dab2adbd8c4f20ae3578
benny.bao <benny.bao@bstek.com>
2011-08-31 17:04:25 +0000

    1. 重构form-layout.js，避免不必要的将控件DOM重新append到DOM树的操作，此类操作导致器中Iframe自动刷新。

--- client/client/scripts/dorado/widget/layout/form-layout.js
+++ client/client/scripts/dorado/widget/layout/form-layout.js
@@ -1,7 +1,7 @@
 (function() {
 
 	var IGNORE_PROPERTIES = ["colSpan", "rowSpan", "align", "vAlign"];
-	
+
 	/**
 	 * @author Benny Bao (mailto:benny.bao@bstek.com)
 	 * @class 表单式布局管理器。
@@ -26,19 +26,21 @@
 	 * @shortTypeName Form
 	 * @extends dorado.widget.layout.Layout
 	 */
-	dorado.widget.layout.FormLayout = $extend(dorado.widget.layout.Layout, /** @scope dorado.widget.layout.FormLayout.prototype */ {
-		$className: "dorado.widget.layout.FormLayout",
-		
-		ATTRIBUTES: /** @scope dorado.widget.layout.FormLayout.prototype */ {
-		
-			className: {
-				defaultValue: "d-form-layout"
+	dorado.widget.layout.FormLayout = $extend(dorado.widget.layout.Layout, /** @scope dorado.widget.layout.FormLayout.prototype */
+	{
+		$className : "dorado.widget.layout.FormLayout",
+
+		ATTRIBUTES : /** @scope dorado.widget.layout.FormLayout.prototype */
+		{
+
+			className : {
+				defaultValue : "d-form-layout"
 			},
-			
-			regionClassName: {
-				defaultValue: "d-form-layout-region"
+
+			regionClassName : {
+				defaultValue : "d-form-layout-region"
 			},
-			
+
 			/**
 			 * 表单布局的分栏方式。
 			 * <p>
@@ -60,123 +62,181 @@
 			 * 	cols: "80[label-region],*[input-region]"
 			 * });
 			 */
-			cols: {
-				defaultValue: "*,*"
+			cols : {
+				defaultValue : "*,*"
 			},
-			
+
 			/**
 			 * 默认的行高。
 			 * @type int
 			 * @attribute
 			 * @default 22
 			 */
-			rowHeight: {
-				defaultValue: 22
+			rowHeight : {
+				defaultValue : 22
 			},
-			
+
 			/**
 			 *
 			 * @type int
 			 * @attribute
 			 * @default 6
 			 */
-			colPadding: {
-				defaultValue: 16
+			colPadding : {
+				defaultValue : 16
 			},
-			
+
 			/**
 			 *
 			 * @type int
 			 * @attribute
 			 * @default 6
 			 */
-			rowPadding: {
-				defaultValue: 6
+			rowPadding : {
+				defaultValue : 6
 			},
-			
-			padding: {
-				defaultValue: 8
+
+			padding : {
+				defaultValue : 8
 			},
-			
+
 			/**
 			 * 是否将表单的宽度自动扩展为撑满容器。
 			 * @type boolean
 			 * @attribute
 			 */
-			stretchWidth: {}
+			stretchWidth : {}
 		},
-		
-		constructor: function() {
+
+		constructor : function() {
 			this._useBlankRow = !(dorado.Browser.safari || dorado.Browser.chrome);
 			$invokeSuper.call(this, arguments);
 		},
-		
-		createDom: function() {
+		createDom : function() {
 			return $DomUtils.xCreateElement({
-				tagName: "TABLE",
-				cellSpacing: 0,
-				cellPadding: 0,
-				content: "^TBODY"
+				tagName : "TABLE",
+				cellSpacing : 0,
+				cellPadding : 0,
+				content : "^TBODY"
 			});
 		},
-		
-		refreshDom: function(dom) {
-			if (this.precalculateRegions) this.precalculateRegions();
-			
-			var tbody = dom.tBodies[0];
-			this._domCache = {};
-			dom.removeChild(tbody);
-			while (tbody.firstChild) {
-				tbody.removeChild(tbody.firstChild);
-			}
-			dom.appendChild(tbody);
-			
-			this.resizeTableAndCols();
-			
-			var grid = this._grid, index = -1, realColWidths = this._realColWidths;
-			if (this._useBlankRow) {
-				var tr = document.createElement("TR");
-				tr.style.height = 0;
-				for (var i = 0; i < realColWidths.length; i++) {
-					var td = document.createElement("TD");
-					td.style.width = realColWidths[i] + "px";
-					tr.appendChild(td);
+		refreshDom : function(dom) {
+
+			function isSameGrid(oldGrid, newGrid) {
+				if(!oldGrid) {
+					return false;
 				}
-				tbody.appendChild(tr);
+				if(oldGrid.length != newGrid.length) {
+					return false;
+				}
+
+				var same = true;
+				for(var i = 0; i < newGrid.length && same; i++) {
+					var oldRow = oldGrid[i], newRow = newGrid[i];
+					if(oldRow.length != newRow.length) {
+						same = false;
+						break;
+					}
+
+					for(var j = 0; j < newRow.length; j++) {
+						var oldRegion = oldRow[j], newRegion = newRow[j];
+						if(oldRegion.colSpan != newRegion.colSpan || oldRegion.rowSpan != newRegion.rowSpan) {
+							same = false;
+							break;
+						}
+					}
+				}
+				return same;
 			}
-			
-			var realignRegions = [];
-			for (var row = 0; row < grid.length; row++) {
-				var tr = document.createElement("TR");
+
+			tbody = dom.tBodies[0];
+			this._domCache = {};
+
+			var grid = this.precalculateRegions();
+			var structureChanged = !isSameGrid(this._grid, grid);
+			if(structureChanged) {
+				this._grid = grid;
+				dom.removeChild(tbody);
+				while(tbody.firstChild) {
+					tbody.removeChild(tbody.firstChild);
+				}
+				dom.appendChild(tbody);
+			} else {
+				grid = this._grid;
+			}
+
+			this.resizeTableAndCols();
+
+			var index, realColWidths = this._realColWidths;
+			if(this._useBlankRow) {
+				if(structureChanged) {
+					var tr = document.createElement("TR");
+					tr.style.height = 0;
+					for(var i = 0; i < realColWidths.length; i++) {
+						var td = document.createElement("TD");
+						td.style.width = realColWidths[i] + "px";
+						tr.appendChild(td);
+					}
+					tbody.appendChild(tr);
+				} else {
+					var tr = tbody.childNodes[0];
+					for(var i = 0; i < realColWidths.length; i++) {
+						var td = tr.childNodes[i];
+						td.style.width = realColWidths[i] + "px";
+					}
+				}
+			}
+
+			var realignRegions = [], rowIndexOffset = (this._useBlankRow) ? 1 : 0;
+			for(var row = 0; row < grid.length; row++) {
+				var tr, index = -1;
+				if(structureChanged) {
+					tr = document.createElement("TR");
+					tbody.appendChild(tr);
+				} else {
+					tr = tbody.childNodes[row + rowIndexOffset];
+				}
 				tr.style.height = this._rowHeight + "px";
-				tbody.appendChild(tr);
-				
+
 				var cols = grid[row], cellForRenders = [];
-				for (var col = 0; col < cols.length; col++) {
+				for(var col = 0; col < cols.length; col++) {
 					var region = grid[row][col];
-					if (region && region.regionIndex <= index) continue;
-					var td = this.createRegionContainer(region);
+					if(region && region.regionIndex <= index) {
+						continue;
+					}
+					index = region.regionIndex;
+					
+					var td;
+					if(structureChanged) {
+						td = this.createRegionContainer(region);
+						tr.appendChild(td);
+					} else {
+						td = tr.childNodes[col];
+					}
+
 					var cls = this._colClss[col];
-					if (cls) $fly(td).addClass(cls);
-					tr.appendChild(td); // real cell
-					if (region) {
+					if(cls) {
+						$fly(td).addClass(cls);
+					}
+					// real cell
+					if(region) {
 						$fly(td).css(region.constraint);
-						
+
 						region.autoWidthAdjust = region.autoHeightAdjust = 0;
-						if ((col + region.colSpan) < cols.length) {
+						if((col + region.colSpan) < cols.length) {
 							region.autoWidthAdjust = 0 - this._colPadding;
 						}
-						if ((row + region.rowSpan) < grid.length) {
+						if((row + region.rowSpan) < grid.length) {
 							region.autoHeightAdjust = 0 - this._rowPadding;
 						}
-						
+
 						td.colSpan = region.colSpan;
 						td.rowSpan = region.rowSpan;
-						
+
 						var w = 0;
-						if (region.colSpan > 1) {
+						if(region.colSpan > 1) {
 							var endIndex = region.colIndex + region.colSpan;
-							for (var j = region.colIndex; j < endIndex; j++) {
+							for(var j = region.colIndex; j < endIndex; j++) {
 								w += realColWidths[j];
 							}
 						} else {
@@ -184,20 +244,20 @@
 						}
 						region.width = w;
 						td.style.width = w + "px";
-						if (dorado.Browser.msie && dorado.Browser.version < '8') td.style.paddingTop = "0px";
+						if(dorado.Browser.msie && dorado.Browser.version < '8')
+							td.style.paddingTop = "0px";
 						td.style.paddingBottom = (-region.autoHeightAdjust || 0) + "px";
-						
+
 						cellForRenders.push({
-							cell: td,
-							region: region
+							cell : td,
+							region : region
 						});
 					}
 				}
-				
-				for (var i = 0; i < cellForRenders.length; i++) {
+
+				for(var i = 0; i < cellForRenders.length; i++) {
 					var cellInfo = cellForRenders[i], td = cellInfo.cell, region = cellInfo.region;
-					index = region.regionIndex;
-					if (region.control._fixedHeight === false) {
+					if(region.control._fixedHeight === false) {
 						var controlDom = region.control.getDom();
 						region.display = controlDom.style.display;
 						controlDom.style.display = "none";
@@ -207,54 +267,57 @@
 					}
 				}
 			}
-			
-			for (var i = 0; i < realignRegions.length; i++) {
-				var region = realignRegions[i], td = this.createRegionContainer(region);
+
+			for(var i = 0; i < realignRegions.length; i++) {
+				var region = realignRegions[i], td = this.getRegionDom(region);
 				region.height = td.clientHeight;
 				controlDom.style.display = region.display;
 				this.renderControl(region, td, true, true);
 			}
 		},
-		
-		createRegionContainer: function(region) {
+		createRegionContainer : function(region) {
 			var dom = this.getRegionDom(region);
-			if (!dom) {
+			if(!dom) {
 				dom = document.createElement("TD");
-				if (region) this._domCache[region.id] = dom;
-			} else if (dom.firstChild) {
+				if(region)
+					this._domCache[region.id] = dom;
+			} else if(dom.firstChild) {
 				dom.removeChild(dom.firstChild);
 			}
 			dom.className = this._regionClassName;
-			
-			if (region) {
+
+			if(region) {
 				var $dom = $fly(dom), constraint = region.constraint;
-				if (constraint.className) $dom.addClass(constraint.className);
-				if (constraint.align) dom.align = constraint.align;
-				if (constraint.vAlign) dom.vAlign = constraint.vAlign;
+				if(constraint.className)
+					$dom.addClass(constraint.className);
+				if(constraint.align)
+					dom.align = constraint.align;
+				if(constraint.vAlign)
+					dom.vAlign = constraint.vAlign;
 				var css = dorado.Object.apply({}, constraint, function(p, v) {
-					if (IGNORE_PROPERTIES.indexOf(p) >= 0) return false;
+					if(IGNORE_PROPERTIES.indexOf(p) >= 0)
+						return false;
 				});
 				$dom.css(css);
 			}
 			return dom;
 		},
-		
-		initColInfos: function() {
+		initColInfos : function() {
 			this._cols = this._cols || "*";
 			var colWidths = this._colWidths = [];
 			var colClss = this._colClss = [];
 			var dynaColCount = 0, fixedWidth = 0;
 			jQuery.each(this._cols.split(','), function(i, col) {
 				var w, cls, ind = col.indexOf('[');
-				if (ind > 0) {
+				if(ind > 0) {
 					w = col.substring(0, ind);
 					cls = col.substring(ind + 1);
 				} else {
 					w = col;
 				}
-				
+
 				colClss[i] = cls;
-				if (w == '*') {
+				if(w == '*') {
 					colWidths.push(-1);
 					dynaColCount++;
 				} else {
@@ -267,123 +330,125 @@
 			this.dynaColCount = dynaColCount;
 			this.fixedWidth = fixedWidth;
 		},
-		
-		precalculateRegions: function() {
-		
-			function precalculateRegion(region) {
-			
+		precalculateRegions : function() {
+
+			function precalculateRegion(grid, region) {
+
 				function doTestRegion() {
-					for (var row = rowIndex; row < rowIndex + rowSpan && row < grid.length; row++) {
-						for (var col = colIndex; col < colIndex + colSpan; col++) {
-							if (grid[row][col]) return false;
+					for(var row = rowIndex; row < rowIndex + rowSpan && row < grid.length; row++) {
+						for(var col = colIndex; col < colIndex + colSpan; col++) {
+							if(grid[row][col])
+								return false;
 						}
 					}
 					return true;
 				}
-				
+
 				var previousRegion = this.getPreviousRegion(region);
 				var pRegionIndex = -1, pRowIndex = 0, pColIndex = -1, pRowSpan = pColSpan = 1;
-				if (previousRegion) {
+				if(previousRegion) {
 					pRegionIndex = previousRegion.regionIndex;
 					pRowIndex = previousRegion.rowIndex;
 					pColIndex = previousRegion.colIndex;
 					pRowSpan = previousRegion.rowSpan;
 					pColSpan = previousRegion.colSpan;
 				}
-				
+
 				var constraint = region.constraint;
 				var rowIndex = pRowIndex, colIndex = pColIndex;
 				var colSpan = ((constraint.colSpan > this.colCount) ? this.colCount : constraint.colSpan) || 1;
 				var rowSpan = constraint.rowSpan || 1;
 				do {
 					colIndex++;
-					if (colIndex + colSpan > this.colCount) {
+					if(colIndex + colSpan > this.colCount) {
 						// go to next row
 						rowIndex++;
 						colIndex = 0;
 					}
-				}
-				while (!doTestRegion());
-				
-				for (var row = 0; row < rowSpan; row++) {
-					if ((rowIndex + row) >= grid.length) grid.push(new Array(this.colCount));
-					for (var col = 0; col < colSpan; col++) {
+				} while (!doTestRegion());
+
+				for(var row = 0; row < rowSpan; row++) {
+					if((rowIndex + row) >= grid.length)
+						grid.push(new Array(this.colCount));
+					for(var col = 0; col < colSpan; col++) {
 						grid[rowIndex + row][colIndex + col] = region;
 					}
 				}
-				
+
 				region.regionIndex = pRegionIndex + 1;
 				region.colIndex = colIndex;
 				region.rowIndex = rowIndex;
 				region.colSpan = colSpan;
 				region.rowSpan = rowSpan;
 			}
-			
+
+
 			this.initColInfos();
-			
-			var grid = this._grid = [];
+
+			var grid = [];
 			var regions = this._regions;
-			for (var it = regions.iterator(); it.hasNext();) {
+			for(var it = regions.iterator(); it.hasNext(); ) {
 				var region = it.next(), constraint = region.constraint;
-				if (constraint == dorado.widget.layout.Layout.NONE_LAYOUT_CONSTRAINT) {
+				if(constraint == dorado.widget.layout.Layout.NONE_LAYOUT_CONSTRAINT) {
 					region.regionIndex = -1;
 					continue;
 				}
 				constraint.colSpan = parseInt(constraint.colSpan);
 				constraint.rowSpan = parseInt(constraint.rowSpan);
-				precalculateRegion.call(this, region);
+				precalculateRegion.call(this, grid, region);
 			}
+			return grid;
 		},
-		
-		resizeTableAndCols: function() {
+		resizeTableAndCols : function() {
 			var realColWidths = this._realColWidths;
-			if (!realColWidths) {
+			if(!realColWidths) {
 				this._realColWidths = realColWidths = [];
 			}
-			
+
 			var table = this.getDom(), padding = parseInt(this._padding) || 0, colPadding = this._colPadding || 0;
 			var containerWidth = (table.parentNode) ? (jQuery(table.parentNode).width() - padding * 2) : 0;
-			if (containerWidth < 0) containerWidth = 0;
-			
+			if(containerWidth < 0)
+				containerWidth = 0;
+
 			var adjust = 0;
-			if (dorado.Browser.safari || dorado.Browser.chrome) {
+			if(dorado.Browser.safari || dorado.Browser.chrome) {
 				adjust = padding * 2;
 			}
-			if (this._stretchWidth || this.dynaColCount > 0) {
+			if(this._stretchWidth || this.dynaColCount > 0) {
 				table.style.width = (containerWidth + adjust) + "px";
 			}
-			if (dorado.Browser.msie && dorado.Browser.version < '8') {
+			if(dorado.Browser.msie && dorado.Browser.version < '8') {
 				table.style.margin = padding + "px";
 			} else {
 				table.style.padding = padding + "px";
 			}
-			
 			containerWidth -= colPadding * (this._colWidths.length - 1);
 			var self = this, changedCols = [];
-			for (var i = 0; i < this._colWidths.length; i++) {
+			for(var i = 0; i < this._colWidths.length; i++) {
 				var w = this._colWidths[i];
-				if (self.dynaColCount > 0) {
-					if (w == -1) {
+				if(self.dynaColCount > 0) {
+					if(w == -1) {
 						w = parseInt((containerWidth - self.fixedWidth) / self.dynaColCount);
 					}
 					w = (w < 0) ? 0 : w;
-				} else if (self._stretchWidth) {
+				} else if(self._stretchWidth) {
 					w = parseInt(w * containerWidth / self.fixedWidth);
 				}
-				if (i < this._colWidths.length - 1) w += colPadding;
-				
-				if (realColWidths[i] != w) changedCols.push(i);
+				if(i < this._colWidths.length - 1)
+					w += colPadding;
+
+				if(realColWidths[i] != w)
+					changedCols.push(i);
 				realColWidths[i] = w;
 			}
 			return changedCols;
 		}
-		
 	});
-	
+
 	var p = dorado.widget.layout.FormLayout.prototype;
 	p.onAddControl = p.onRemoveControl = p.doRefreshRegion = function() {
-		if (!this._attached || this._disableRendering) return;
+		if(!this._attached || this._disableRendering)
+			return;
 		this.refreshDom(this.getDom());
 	};
-	
 })();
