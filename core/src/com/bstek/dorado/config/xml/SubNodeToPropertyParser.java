0acd566f981a3064880c3150e4391bfdaad24f49
benny.bao <benny.bao@bstek.com>
2011-11-07 05:33:52 +0000

    修正近期出现的Shell、Desktop控件不可使用的BUG。本次修正过程中微调了Shell中Desktop子节点的定义方式，移除了原先的<Desktop>虚节点，今后<DesktopCarousel>应直接放置在<Shell>，而不是放在<Desktop>虚节点下。

--- core/src/com/bstek/dorado/config/xml/SubNodeToPropertyParser.java
+++ core/src/com/bstek/dorado/config/xml/SubNodeToPropertyParser.java
@@ -1,8 +1,12 @@
 package com.bstek.dorado.config.xml;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.beanutils.PropertyUtils;
+import org.springframework.util.ClassUtils;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
@@ -13,7 +17,34 @@ import com.bstek.dorado.config.definition.DefinitionInitOperation;
 import com.bstek.dorado.util.Assert;
 
 public class SubNodeToPropertyParser extends ConfigurableDispatchableXmlParser {
+	private XmlParserAnnotationHelper xmlParserAnnotationHelper;
+	private boolean initialized = false;
+	private List<Class<?>> implTypes = new ArrayList<Class<?>>();
 	private String property;
+	private Class<?> elementType;
+
+	public void setXmlParserAnnotationHelper(
+			XmlParserAnnotationHelper xmlParserAnnotationHelper) {
+		this.xmlParserAnnotationHelper = xmlParserAnnotationHelper;
+	}
+
+	public void registerImplType(Class<?> implType) {
+		if (initialized) {
+			throw new IllegalStateException(
+					"Can not registerImpleType after CollectionParser used.");
+		}
+		implTypes.add(implType);
+	}
+
+	public void setImplTypes(List<Class<?>> implTypes) {
+		for (Class<?> implType : implTypes) {
+			registerImplType(implType);
+		}
+	}
+
+	public List<Class<?>> getImplTypes() {
+		return implTypes;
+	}
 
 	/**
 	 * @return the property
@@ -23,14 +54,55 @@ public class SubNodeToPropertyParser extends ConfigurableDispatchableXmlParser {
 	}
 
 	/**
-	 * @param property the property to set
+	 * @param property
+	 *            the property to set
 	 */
 	public void setProperty(String property) {
 		this.property = property;
 	}
 
+	public void setElementType(Class<?> elementType) {
+		this.elementType = elementType;
+	}
+
+	public void setElementType(String elementType)
+			throws ClassNotFoundException, LinkageError {
+		this.elementType = ClassUtils.forName(elementType,
+				ClassUtils.getDefaultClassLoader());
+	}
+
+	public Class<?> getElementType() {
+		return elementType;
+	}
+
+	public void init() throws Exception {
+		if (!initialized) {
+			initialized = true;
+
+			Map<String, XmlParser> subParsers = getSubParsers();
+			Map<String, XmlParser> oldSubParsers = new LinkedHashMap<String, XmlParser>(
+					subParsers);
+			subParsers.clear();
+
+			for (Class<?> implType : implTypes) {
+				String nodeName = implType.getSimpleName();
+				TypeAnnotationInfo typeAnnotationInfo = xmlParserAnnotationHelper
+						.getTypeAnnotationInfo(implType);
+				if (typeAnnotationInfo != null) {
+					nodeName = typeAnnotationInfo.getNodeName();
+				}
+				if (!subParsers.containsKey(nodeName)) {
+					registerSubParser(nodeName, typeAnnotationInfo.getParser());
+				}
+			}
+			subParsers.putAll(oldSubParsers);
+		}
+	}
+
 	@Override
 	protected Object doParse(Node node, ParseContext context) throws Exception {
+		init();
+
 		List<?> list = this.dispatchChildElements((Element) node, context);
 		return (list != null && !list.isEmpty()) ? new SetElementOperation(
 				property, list.get(0)) : null;
@@ -52,8 +124,7 @@ class SetElementOperation implements DefinitionInitOperation {
 		if (object instanceof Definition) {
 			Definition parentDefinition = (Definition) object;
 			parentDefinition.setProperty(property, element);
-		}
-		else {
+		} else {
 			PropertyUtils.setProperty(object, property, element);
 		}
 	}
