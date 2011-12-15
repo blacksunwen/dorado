267976c4499f79263a9f35689e9338607b9c4f17
benny.bao <benny.bao@bstek.com>
2011-12-15 16:23:44 +0000

    新建touch子项目

---
+++ touch/src/com/bstek/dorado/touch/TouchView.java
@@ -0,0 +1,11 @@
+package com.bstek.dorado.touch;
+
+import com.bstek.dorado.view.View;
+
+/**
+ * @author Benny Bao (mailto:benny.bao@bstek.com)
+ * @since 2011-12-16
+ */
+public class TouchView extends View {
+
+}
