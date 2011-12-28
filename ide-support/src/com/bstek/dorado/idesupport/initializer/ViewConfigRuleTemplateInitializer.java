6f03a064f5270bea64ab095839f1460b23626333
benny.bao <benny.bao@bstek.com>
2011-12-28 08:03:35 +0000

    # 修正7.0.2中刚刚出现的view.xml保存后无法再打开的BUG # 修正7.0.1中开始出现的IDE中Action下不显示parameter属性的

--- ide-support/src/com/bstek/dorado/idesupport/initializer/ViewConfigRuleTemplateInitializer.java
+++ ide-support/src/com/bstek/dorado/idesupport/initializer/ViewConfigRuleTemplateInitializer.java
@@ -85,6 +85,7 @@ public class ViewConfigRuleTemplateInitializer implements
 			}
 			componentRuleTemplate.setSortFactor(++sortFactor);
 			componentRuleTemplate.setCategory(registerInfo.getCategory());
+			componentRuleTemplate.setNodeName(registerInfo.getName());
 
 			if (registerInfo instanceof AssembledComponentTypeRegisterInfo) {
 				AssembledComponentTypeRegisterInfo assembledComponentTypeRegisterInfo = (AssembledComponentTypeRegisterInfo) registerInfo;
