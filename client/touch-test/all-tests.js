945c2f9500ec2ff81f96cd769847a21c867a8d7f
frank.zhang <frank.zhang@bstek.com>
2011-12-31 05:07:32 +0000

    touch first commit.

---
+++ client/touch-test/all-tests.js
@@ -0,0 +1,55 @@
+var ALL_JS_PAGES = [
+	"client/touch-test/base/button-test.html",
+	"client/touch-test/base/toolbar-test.html",
+	"client/touch-test/base/panel-test.html",
+    "client/touch-test/form/form-test.html",
+
+	"client/touch-test/base/float-panel-test.html",
+	"client/touch-test/util/task-indicator-test.html",
+	"client/touch-test/base/groupbox-test.html",
+	"client/touch-test/base/accordion-test.html",
+
+    "client/touch-test/base/dialog-test.html",
+    "client/touch-test/base/msgbox-test.html",
+	"client/touch-test/base/notifytip-test.html",
+
+	"client/touch-test/base/tabbar-test.html",
+	"client/touch-test/base/card-book-test.html",
+    "client/touch-test/base/tabcontrol-test.html",
+
+    "client/touch-test/base/splitpanel-test.html",
+    "client/touch-test/base/progressbar-test.html",
+    "client/touch-test/base/slider-test.html",
+    "client/touch-test/base/tip-test.html",
+
+	"client/touch-test/form/data-label-test.html",
+	"client/touch-test/form/text-editor-test.html",
+	"client/touch-test/form/data-text-editor-test.html",
+	"client/touch-test/form/text-area-test.html",
+	"client/touch-test/form/list-dropdown-test.html",
+    "client/touch-test/form/check-box-test.html",
+    "client/touch-test/form/radio-button-test.html",
+	"client/touch-test/form/auto-mapvalues-dropdown-test.html",
+    "client/touch-test/form/ym-dropdown-test.html",
+    "client/touch-test/form/date-dropdown-test.html",
+	"client/touch-test/form/form-element-test.html",
+	"client/touch-test/form/data-form-element-test.html",
+
+	"client/touch-test/form/spinner-test.html",
+	"client/touch-test/form/data-message-test.html",
+	
+	"client/touch-test/data-control/data-pilot-test.html",
+
+	"client/touch-test/list/list-box-test.html",
+	"client/touch-test/list/data-list-box-test.html",
+
+	"client/touch-test/grid/column-model-test.html",
+	"client/touch-test/grid/grid1-test.html",
+	"client/touch-test/grid/grid2-test.html",
+	"client/touch-test/grid/grid3-test.html",
+	"client/touch-test/grid/grid4-test.html",
+	"client/touch-test/grid/data-grid-test.html",
+
+	"client/touch-test/tree/tree-test.html",
+	"client/touch-test/tree/data-tree-test.html"
+];
