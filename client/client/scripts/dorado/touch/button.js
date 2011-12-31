48627bb7ee774b7c072a71cde293c33feed17f2c
frank.zhang <frank.zhang@bstek.com>
2011-12-31 05:07:52 +0000

    touch first commit.

---
+++ client/client/scripts/dorado/touch/button.js
@@ -0,0 +1,124 @@
+dorado.touch.Button = $extend(dorado.widget.Control, {
+    _inherentClassName: "button",
+    ATTRIBUTES: {
+        className: {
+            defaultValue: "button-plain"
+        },
+
+        caption: {},
+
+        disabled: {},
+
+        ui: {},
+
+        icon: {},
+
+        iconClass: {},
+
+        //top bottom left right
+        iconAlign: {
+            defaultValue: "left"
+        },
+
+        badgeCls: {},
+
+        badgeText: {}
+    },
+
+    EVENTS: {
+        onTap: {}
+    },
+
+    onTap: function() {
+        var button = this;
+        button.fireEvent("onTap", button);
+    },
+
+    createDom: function() {
+        var button = this, doms = {}, dom = $DomUtils.xCreate({
+            tagName: "span",
+            className: this._className,
+            content: {
+                tagName: "span",
+                className: "label",
+                contextKey: "label",
+                content: this._caption
+            }
+        }, null, doms);
+
+        button._doms = doms;
+        $fly(dom).bind("tap", function() {
+            button.onTap();
+        });
+
+        $fly(dom).addClass("button-iconalign-" + button._iconAlign).addClass(button._ui || "");
+
+        button.doCreateIcon(dom);
+        button.doCreateBadge(dom);
+
+        return dom;
+    },
+
+    refreshDom: function(dom) {
+        $invokeSuper.call(this, arguments);
+        var button = this, doms = button._doms, icon = button._icon, iconClass = button._iconClass;
+
+        if (icon || iconClass) {
+            if (!doms.icon) {
+                button.doCreateIcon();
+            } else {
+                $fly(doms.icon).css("display", "");
+            }
+        } else if (doms.icon) {
+            $fly(doms.icon).css("display", "none");
+        }
+
+        doms.label.innerHTML = button._caption || "";
+
+        if (icon)
+            doms.icon.src = icon;
+        else if (doms.icon)
+            doms.icon.src = "data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==";
+
+        if (iconClass) {
+            $fly(doms.icon).addClass(iconClass);
+        }
+
+        if (button._badgeText) {
+            $fly(dom).addClass("button-hasbadge");
+            if (!doms.badge) {
+                button.doCreateBadge();
+            } else {
+                $fly(doms.badge).css("display", "");
+            }
+        } else if (doms.badge) {
+            $fly(dom).removeClass("button-hasbadge");
+            $fly(doms.badge).css("display", "none");
+        }
+    },
+
+    doCreateBadge: function(dom) {
+        var button = this, doms = button._doms, badgeText = button._badgeText;
+        if (badgeText == null) {
+            return;
+        }
+        dom = dom ? dom : button._dom;
+        var badgeEl = document.createElement("span");
+        badgeEl.className = "badge";
+        badgeEl.innerHTML = badgeText;
+        doms.badge = badgeEl;
+        dom.appendChild(badgeEl);
+    },
+
+    doCreateIcon: function(dom) {
+        var button = this, doms = button._doms, icon = button._icon, iconClass = button._iconClass;
+        if (icon == null && iconClass == null) {
+            return;
+        }
+        dom = dom ? dom : button._dom;
+        var iconEl = document.createElement("img");
+        iconEl.className = "icon";
+        doms.icon = iconEl;
+        dom.appendChild(iconEl);
+    }
+});
