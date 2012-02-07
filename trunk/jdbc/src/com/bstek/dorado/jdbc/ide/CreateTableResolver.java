c1d10e87261714b22a900af5124da2673b0b24d1
mark.li@bstek.com <mark.li@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2012-02-07 02:39:30 +0000

    为IDE提供Service

--- trunk/jdbc/src/com/bstek/dorado/jdbc/ide/CreateTableResolver.java
+++ trunk/jdbc/src/com/bstek/dorado/jdbc/ide/CreateTableResolver.java
@@ -3,7 +3,10 @@ package com.bstek.dorado.jdbc.ide;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
 
 import com.bstek.dorado.jdbc.JdbcEnviroment;
 import com.bstek.dorado.jdbc.JdbcUtils;
@@ -18,16 +21,32 @@ public class CreateTableResolver extends Resolver {
 		String catalog = request.getParameter(PARAM_CATA);
 		String schema  = request.getParameter(PARAM_SCHE);
 		String table   = request.getParameter(PARAM_TBNM);
+		String xml = request.getParameter(PARAM_XML);
 		
 		JdbcEnviroment jdbcEnv = JdbcUtils.getEnviromentManager().getEnviroment(envName);
 		
-		return toContent(catalog, schema, table, jdbcEnv);
+		Document document = null;
+		if (StringUtils.isNotEmpty(xml)) {
+			try {
+				document = DocumentHelper.parseText(xml);
+			} catch (DocumentException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		return toContent(catalog, schema, table, jdbcEnv, document);
 	}
 
-	public String toContent(String catalog, String schema, String table, final JdbcEnviroment jdbcEnv) {
+	public String toContent(String catalog, String schema, String table, 
+			final JdbcEnviroment jdbcEnv, Document document) {
 		final ModelGeneratorSuit generator = jdbcEnv.getModelGeneratorSuit();
 		
-		Document document = generator.getTableMetaDataGenerator().createDocument(catalog, schema, table, jdbcEnv);
+		if (document == null) {
+			document = generator.getTableMetaDataGenerator().createDocument(catalog, schema, table, jdbcEnv);
+		} else {
+			document = generator.getTableMetaDataGenerator().mergeDocument(catalog, schema, table, jdbcEnv, document);
+		}
+		 
 		return toString(document);
 	}
 	
