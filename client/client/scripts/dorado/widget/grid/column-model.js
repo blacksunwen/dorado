100a1e384db5ca9c61a0454efe706661cec1390b
benny.bao <benny.bao@bstek.com>
2012-02-23 16:45:31 +0000

    修正column.wrappable不生效的BUG

--- client/client/scripts/dorado/widget/grid/column-model.js
+++ client/client/scripts/dorado/widget/grid/column-model.js
@@ -778,7 +778,7 @@
 	dorado.widget.grid.DefaultCellRenderer = $extend(dorado.widget.grid.CellRenderer, /** @scope dorado.widget.grid.DefaultCellRenderer.prototype */{
 		doRender: function(dom, arg) {
 			dom.innerText = this.getText(arg.data, arg.column);
-			$fly(dom).toggleClass("wrappable", !!arg.column._wrappable);
+			$fly(dom.parentNode).toggleClass("wrappable", !!arg.column._wrappable);
 			this.renderFlag(dom, arg);
 		}
 	});
