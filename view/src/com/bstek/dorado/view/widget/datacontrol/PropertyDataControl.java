0e1938a9dca6f0e0194710c56e3f122a1d14813a
benny.bao <benny.bao@bstek.com>
2012-04-20 08:07:07 +0000

    OPEN - issue DORADOSEVEN-734: TextEditor的onGetBindingData事件 http://jira.bsdn.org/browse/DORADOSEVEN-734

--- view/src/com/bstek/dorado/view/widget/datacontrol/PropertyDataControl.java
+++ view/src/com/bstek/dorado/view/widget/datacontrol/PropertyDataControl.java
@@ -1,5 +1,10 @@
 package com.bstek.dorado.view.widget.datacontrol;
 
+import com.bstek.dorado.annotation.ClientEvent;
+import com.bstek.dorado.annotation.ClientEvents;
+
+@ClientEvents({ @ClientEvent(name = "onGetBindingData"),
+		@ClientEvent(name = "onGetBindingDataType") })
 public interface PropertyDataControl extends DataControl {
 	/**
 	 * @return the property
@@ -7,7 +12,8 @@ public interface PropertyDataControl extends DataControl {
 	public String getProperty();
 
 	/**
-	 * @param property the property to set
+	 * @param property
+	 *            the property to set
 	 */
 	public void setProperty(String property);
 }
