5349582085d34ee216e8607de7f6d489951446b7
benny.bao <benny.bao@bstek.com>
2012-05-03 08:16:41 +0000

    RESOLVED - issue DORADOSEVEN-667: IE7,8下DataTree一次性展开4层，报stackoverflow错误 http://jira.bsdn.org/browse/DORADOSEVEN-667

--- client/client/scripts/dorado/widget/tree/tree-model.js
+++ client/client/scripts/dorado/widget/tree/tree-model.js
@@ -117,7 +117,7 @@ dorado.widget.tree.Node = $extend([dorado.AttributeSupport, dorado.EventSupport]
 		nodes: {
 			setter: function(nodes) {
 				this.clearChildren();
-				this.addNodes(nodes);
+				this.addNodes.call(this, nodes);
 			}
 		},
 		
