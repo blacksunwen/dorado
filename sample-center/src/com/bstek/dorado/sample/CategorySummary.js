b704c40a6bc4184372c0c4f74e93b84533f63f7b
benny.bao <benny.bao@bstek.com>
2012-08-07 10:04:12 +0000

    dorado7-831	Dialog中使用Tabset渲染问题 dorado7-827	datasetDropdown 加入datacolum后下拉框弹不出来 dorado7-824	TextAreaEditor绑定一个普通的Trigger时，用于触发Trigger动作的按钮在TextArea上不会显示 dorado7-828	当view同时加载多个js文件，匿名事件的写法调用不正确 #dorado7-821 dorado.util.AjaxEngine的一个问题 为SampleCenter添加hash处理

---
+++ sample-center/src/com/bstek/dorado/sample/CategorySummary.js
@@ -0,0 +1,5 @@
+// @Global
+function showURL(id) {
+	dorado.MessageBox.alert("http://bsdn.org/projects/dorado7/deploy/sample-center/com.bstek.dorado.sample.Main.d" +
+		"#category" + id);
+}
--- sample-center/src/com/bstek/dorado/sample/Main.java
+++ sample-center/src/com/bstek/dorado/sample/Main.java
@@ -1,13 +1,16 @@
 package com.bstek.dorado.sample;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.stereotype.Component;
 
 import com.bstek.dorado.annotation.DataProvider;
+import com.bstek.dorado.annotation.Expose;
 import com.bstek.dorado.data.provider.Page;
 import com.bstek.dorado.sample.dao.ExampleCategoryDao;
 import com.bstek.dorado.sample.dao.ExampleDao;
@@ -16,6 +19,7 @@ import com.bstek.dorado.sample.entity.ExampleCategory;
 
 @Component
 public class Main {
+	private static final Long QUICK_START_CATEGORY = 37000L;
 	private static final Long RECENT_EXAMPLE_CATEGORY = -100L;
 
 	@Resource
@@ -53,5 +57,35 @@ public class Main {
 					+ "where e.id = r.exampleId and r.categoryId = "
 					+ categoryId + " order by r.sortFlag");
 		}
+	}
+
+	@Expose
+	public Long[] getPath(String type, long id) {
+		List<Long> path = new ArrayList<Long>();
+		Long categoryId = null;
+
+		if ("example".equals(type)) {
+			List<Long> categoryIds = exampleDao
+					.find("select categoryId from CategoryExampleRelation where exampleId="
+							+ id + " and categoryId <> " + QUICK_START_CATEGORY);
+			if (!categoryIds.isEmpty()) {
+				categoryId = categoryIds.get(0);
+			}
+		} else if ("category".equals(type)) {
+			categoryId = exampleCategoryDao
+					.findUnique("select categoryId from ExampleCategory where id="
+							+ id);
+		}
+
+		while (categoryId != null && categoryId > 0) {
+			path.add(categoryId);
+			categoryId = exampleCategoryDao
+					.findUnique("select categoryId from ExampleCategory where id="
+							+ categoryId);
+		}
+
+		Long[] pathArray = path.toArray(new Long[0]);
+		ArrayUtils.reverse(pathArray);
+		return pathArray;
 	}
 }
