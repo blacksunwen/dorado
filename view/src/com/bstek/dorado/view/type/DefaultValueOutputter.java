d55259e6c7e1ba4e0c991764ed1f99761fd13946
benny.bao <benny.bao@bstek.com>
2012-08-14 10:19:32 +0000

    修正13版本中输入默认值导致类型不匹配或报NullPointException的BUG

--- view/src/com/bstek/dorado/view/type/DefaultValueOutputter.java
+++ view/src/com/bstek/dorado/view/type/DefaultValueOutputter.java
@@ -2,6 +2,8 @@ package com.bstek.dorado.view.type;
 
 import com.bstek.dorado.data.type.DataType;
 import com.bstek.dorado.data.type.property.PropertyDef;
+import com.bstek.dorado.view.output.DataOutputter;
+import com.bstek.dorado.view.output.JsonBuilder;
 import com.bstek.dorado.view.output.OutputContext;
 import com.bstek.dorado.view.output.VirtualPropertyOutputter;
 
@@ -9,7 +11,8 @@ import com.bstek.dorado.view.output.VirtualPropertyOutputter;
  * @author Benny Bao (mailto:benny.bao@bstek.com)
  * @since 2012-8-3
  */
-public class DefaultValueOutputter implements VirtualPropertyOutputter {
+public class DefaultValueOutputter extends DataOutputter implements
+		VirtualPropertyOutputter {
 
 	public void output(Object object, String property, OutputContext context)
 			throws Exception {
@@ -17,8 +20,13 @@ public class DefaultValueOutputter implements VirtualPropertyOutputter {
 		Object defaultValue = propertyDef.getDefaultValue();
 		if (defaultValue != null) {
 			DataType dataType = propertyDef.getDataType();
-			defaultValue = dataType.fromObject(defaultValue);
-			context.getJsonBuilder().key("defaultValue").value(defaultValue);
+			if (dataType != null) {
+				defaultValue = dataType.fromObject(defaultValue);
+			}
+
+			JsonBuilder jsonBuilder = context.getJsonBuilder();
+			jsonBuilder.key("defaultValue");
+			outputData(defaultValue, context);
 		}
 	}
 
