6e53157ea55eb497a976bd1be1e0e4aaf8758cae
benny.bao <benny.bao@bstek.com>
2012-10-10 05:51:14 +0000

    改进ControlMenuItem的继承关系

--- view/src/com/bstek/dorado/view/widget/base/menu/MenuItem.java
+++ view/src/com/bstek/dorado/view/widget/base/menu/MenuItem.java
@@ -1,6 +1,11 @@
 package com.bstek.dorado.view.widget.base.menu;
 
+import java.util.List;
+
 import com.bstek.dorado.annotation.ClientObject;
+import com.bstek.dorado.annotation.ClientProperty;
+import com.bstek.dorado.annotation.XmlSubNode;
+import com.bstek.dorado.view.widget.InnerElementList;
 
 /**
  * @author Benny Bao (mailto:benny.bao@bstek.com)
@@ -9,4 +14,27 @@ import com.bstek.dorado.annotation.ClientObject;
 @ClientObject(prototype = "dorado.widget.menu.MenuItem",
 		shortTypeName = "Default")
 public class MenuItem extends TextMenuItem implements MenuItemGroup {
+
+	private List<BaseMenuItem> menuItems = new InnerElementList<BaseMenuItem>(
+			this);
+
+	public void addItem(BaseMenuItem menuItem) {
+		menuItems.add(menuItem);
+	}
+
+	public BaseMenuItem getItem(String name) {
+		for (BaseMenuItem item : menuItems) {
+			if (name.equals(item.getName())) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	@XmlSubNode
+	@ClientProperty
+	public List<BaseMenuItem> getItems() {
+		return menuItems;
+	}
+
 }
