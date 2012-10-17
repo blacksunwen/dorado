c45156c6ff930df254fc9f976665b0dc6318983c
benny.bao@bstek.com <benny.bao@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2012-10-17 18:33:42 +0000

    SVN迁移

--- trunk/addon-builder/builder/com/bstek/dorado/addonbuilder/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.bstek.dorado.addonbuilder;
-
-public class Constants {
-	public static final String VERSION = "%version%";
-	public static final String FULL_VERSION = "%fullVersion%";
-}
