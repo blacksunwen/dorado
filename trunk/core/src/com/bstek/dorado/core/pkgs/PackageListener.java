a424153daeffb0a030681a397e0eb32353e87a78
benny.bao@bstek.com <benny.bao@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2012-10-23 07:13:12 +0000

    改进PackageConfigurer和PackageListener接口 删除dorado-core中的portal控件

--- trunk/core/src/com/bstek/dorado/core/pkgs/PackageListener.java
+++ trunk/core/src/com/bstek/dorado/core/pkgs/PackageListener.java
@@ -1,12 +1,16 @@
 package com.bstek.dorado.core.pkgs;
 
+import com.bstek.dorado.core.io.ResourceLoader;
+
 /**
  * @author Benny Bao (mailto:benny.bao@bstek.com)
  * @since 2012-10-16
  */
 public interface PackageListener {
 
-	public void beforeLoadPackage(PackageInfo packageInfo) throws Exception;
+	public void beforeLoadPackage(PackageInfo packageInfo,
+			ResourceLoader resourceLoader) throws Exception;
 
-	public void afterLoadPackage(PackageInfo packageInfo) throws Exception;
+	public void afterLoadPackage(PackageInfo packageInfo,
+			ResourceLoader resourceLoader) throws Exception;
 }
