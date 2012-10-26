25574443c9a931a2665e54fa50d6b86c900c5877
benny.bao@bstek.com <benny.bao@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2012-10-26 16:57:39 +0000

    SVN迁移

--- trunk/sample-center/web/libraries/syntax-highlighter/jquery.beautyOfCode.init.js
+++
@@ -1,7 +0,0 @@
-jQuery.beautyOfCode.init({
-	autoLoad: false,
-	baseUrl: $setting["common.contextPath"],
-	scripts: "/libraries/syntax-highlighter/",
-	styles: "/libraries/syntax-highlighter/",
-	brushes: ["Xml", "Java", "JScript"]
-});
