ee92b98349b71f3cda8b67dccfde989a60a6ec0d
benny.bao <benny.bao@bstek.com>
2012-11-05 08:46:23 +0000

    微调License声明

--- core/test/com/bstek/dorado/config/xml/drink/Wine.java
+++ core/test/com/bstek/dorado/config/xml/drink/Wine.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Dorado 7.x (http://dorado7.bsdn.org).
  * 
- * Copyright (c) 2011-2012 BSTEK Information Technology Limited. All rights reserved.
+ * Copyright (c) 2002-2012 BSTEK Corp. All rights reserved.
  * 
  * This file is dual-licensed under the AGPLv3 (http://www.gnu.org/licenses/agpl-3.0.html) 
  * and BSDN commercial (http://www.bsdn.org/licenses) licenses.
