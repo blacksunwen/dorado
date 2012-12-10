e316ee05109b0777efa799a3977efe926cdf88c7
william.jiang@bstek.com <william.jiang@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2012-12-10 08:08:22 +0000

    修正trigger下拉框中无法看到autoOpenMappingDropDown1和autoOpenMappingDropDown2的问题

--- trunk/view/src/com/bstek/dorado/view/widget/form/FormElement.java
+++ trunk/view/src/com/bstek/dorado/view/widget/form/FormElement.java
@@ -138,7 +138,7 @@ public class FormElement extends Control implements FormConfig,
 
 	@ComponentReference("Trigger")
 	@IdeProperty(
-			enumValues = "triggerClear,autoMappingDropDown1,autoMappingDropDown2,defaultDateDropDown,defaultDateTimeDropDown,defaultYearMonthDropDown")
+			enumValues = "triggerClear,autoMappingDropDown1,autoMappingDropDown2,autoOpenMappingDropDown1,autoOpenMappingDropDown2,defaultDateDropDown,defaultDateTimeDropDown,defaultYearMonthDropDown")
 	public String getTrigger() {
 		return trigger;
 	}
