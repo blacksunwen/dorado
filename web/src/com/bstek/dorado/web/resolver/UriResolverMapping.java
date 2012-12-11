b251325243904080a32f9b2295f03aca89c9a3f6
benny.bao <benny.bao@bstek.com>
2012-12-11 07:37:52 +0000

    修正UriResolverMapping无法支持interceptor的BUG

--- web/src/com/bstek/dorado/web/resolver/UriResolverMapping.java
+++ web/src/com/bstek/dorado/web/resolver/UriResolverMapping.java
@@ -13,10 +13,14 @@
 package com.bstek.dorado.web.resolver;
 
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.servlet.handler.AbstractUrlHandlerMapping;
 
 import com.bstek.dorado.util.Assert;
@@ -26,23 +30,47 @@ import com.bstek.dorado.util.Assert;
  * @since 2010-7-15
  */
 public class UriResolverMapping extends AbstractUrlHandlerMapping {
+	private static final Log logger = LogFactory
+			.getLog(UriResolverMapping.class);
 
 	@Override
 	protected Object lookupHandler(String urlPath, HttpServletRequest request)
 			throws Exception {
 		Map<String, Object> handlerMap = getHandlerMap();
 		Object handler = handlerMap.get(urlPath);
+		String bestPatternMatch = urlPath;
+		String pathWithinMapping = urlPath;
+
 		if (handler == null) {
+			PathMatcher pathMatcher = getPathMatcher();
 			for (Iterator<String> it = handlerMap.keySet().iterator(); it
 					.hasNext();) {
 				String registeredPath = it.next();
-				if (getPathMatcher().match(registeredPath, urlPath)) {
+				if (pathMatcher.match(registeredPath, urlPath)) {
 					request.setAttribute("originalUrlPath", urlPath);
 					handler = handlerMap.get(registeredPath);
+					bestPatternMatch = registeredPath;
+					pathWithinMapping = pathMatcher.extractPathWithinPattern(
+							bestPatternMatch, urlPath);
 					break;
 				}
 			}
 		}
+
+		if (handler != null) {
+			PathMatcher pathMatcher = getPathMatcher();
+			Map<String, String> uriTemplateVariables = new LinkedHashMap<String, String>();
+			uriTemplateVariables.putAll(pathMatcher
+					.extractUriTemplateVariables(bestPatternMatch, urlPath));
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("URI Template variables for request [" + urlPath
+						+ "] are " + uriTemplateVariables);
+			}
+			handler = buildPathExposingHandler(handler, bestPatternMatch,
+					pathWithinMapping, uriTemplateVariables);
+		}
+
 		return handler;
 	}
 
