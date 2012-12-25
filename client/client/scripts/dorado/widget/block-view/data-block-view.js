0c60e33acb64b339dbd352029b5821fd92cc920f
benny.bao <benny.bao@bstek.com>
2012-12-25 18:30:46 +0000

    #dorado7-1607 DataBlockView控件间的拖拽

--- client/client/scripts/dorado/widget/block-view/data-block-view.js
+++ client/client/scripts/dorado/widget/block-view/data-block-view.js
@@ -1,15 +1,14 @@
 /*
  * This file is part of Dorado 7.x (http://dorado7.bsdn.org).
- * 
+ *
  * Copyright (c) 2002-2012 BSTEK Corp. All rights reserved.
- * 
- * This file is dual-licensed under the AGPLv3 (http://www.gnu.org/licenses/agpl-3.0.html) 
+ *
+ * This file is dual-licensed under the AGPLv3 (http://www.gnu.org/licenses/agpl-3.0.html)
  * and BSDN commercial (http://www.bsdn.org/licenses) licenses.
- * 
+ *
  * If you are unsure which license is appropriate for your use, please contact the sales department
  * at http://www.bstek.com/contact.
  */
-
 /**
  * @author Benny Bao (mailto:benny.bao@bstek.com)
  * @component Collection
@@ -208,7 +207,7 @@ dorado.widget.DataBlockView = $extend([dorado.widget.AbstractBlockView, dorado.w
 			case dorado.widget.DataSet.MESSAGE_CURRENT_CHANGED:
 				if (arg.entityList == this._itemModel.getItems()) {
 					var oldCurrentEntity = this.getCurrentItem();
-					if (!oldCurrentEntity || oldCurrentEntity.page.pageNo != arg.entityList.pageNo) {
+					if (!oldCurrentEntity || (oldCurrentEntity.page && oldCurrentEntity.page.pageNo != arg.entityList.pageNo)) {
 						this.refresh(true);
 					} else {
 						this.setCurrentEntity(arg.entityList.current);
