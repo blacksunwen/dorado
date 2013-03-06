9f794a44eb52ab71be7421fe3f427c2d63d3d668
alex.tong@bstek.com <alex.tong@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2013-03-06 07:51:20 +0000

    1.添加首页。 2.菜单动态化，位以后的权限控制做准备。 3.认证模式和执行日志输出器等部分代码进行了重构 4.提交了Test代码

--- trunk/console/src/com/bstek/dorado/console/parser/ParserTree.java
+++ trunk/console/src/com/bstek/dorado/console/parser/ParserTree.java
@@ -1,3 +1,15 @@
+/*
+ * This file is part of Dorado 7.x (http://dorado7.bsdn.org).
+ * 
+ * Copyright (c) 2002-2012 BSTEK Corp. All rights reserved.
+ * 
+ * This file is dual-licensed under the AGPLv3 (http://www.gnu.org/licenses/agpl-3.0.html) 
+ * and BSDN commercial (http://www.bsdn.org/licenses) licenses.
+ * 
+ * If you are unsure which license is appropriate for your use, please contact the sales department
+ * at http://www.bstek.com/contact.
+ */
+
 package com.bstek.dorado.console.parser;
 
 import java.util.ArrayList;
@@ -9,7 +21,12 @@ import com.bstek.dorado.config.xml.XmlParser;
 import com.bstek.dorado.config.xml.XmlParserHelper;
 import com.bstek.dorado.config.xml.XmlParserHelper.XmlParserInfo;
 import com.bstek.dorado.util.clazz.ClassUtils;
-
+/**
+ * 
+ *
+ * @author Alex Tong(mailto:alex.tong@bstek.com)
+ * @since  2013-3-4
+ */
 public class ParserTree {
 
 	@DataProvider
