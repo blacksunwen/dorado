f74c4006e8e4a0c27f19830fd4f2b12fe1d8b3c7
benny.bao <benny.bao@bstek.com>
2013-05-21 08:06:22 +0000

    修正动态私有DataType不能直接赋值给DataSet的BUG

--- view/src/com/bstek/dorado/view/type/DataTypePropertyOutputter.java
+++ view/src/com/bstek/dorado/view/type/DataTypePropertyOutputter.java
@@ -19,7 +19,6 @@ import org.springframework.util.StringUtils;
 
 import com.bstek.dorado.data.type.AggregationDataType;
 import com.bstek.dorado.data.type.DataType;
-import com.bstek.dorado.util.proxy.BeanExtender;
 import com.bstek.dorado.view.output.JsonBuilder;
 import com.bstek.dorado.view.output.ObjectOutputterDispatcher;
 import com.bstek.dorado.view.output.OutputContext;
@@ -47,46 +46,48 @@ public class DataTypePropertyOutputter extends ObjectOutputterDispatcher {
 		JsonBuilder json = context.getJsonBuilder();
 		json.beginValue();
 		Writer writer = context.getWriter();
-		if (BeanExtender.getExProperty(dataType, "dorado.dynamicDataType") != null) {
-			writer.append("dorado.DataTypeRepository.parseSingleDataType(");
-			outputObject(dataType, context);
+
+		// if (BeanExtender.getExProperty(dataType, "dorado.dynamicDataType") !=
+		// null) {
+		// writer.append("dorado.DataTypeRepository.parseSingleDataType(");
+		// outputObject(dataType, context);
+		// writer.append(")");
+		// } else {
+		if (useLazyDataType) {
+			writer.append("dorado.LazyLoadDataType.create(v.dataTypeRepository,");
+		}
+
+		writer.append('"');
+		String id = dataType.getId();
+		if (dataType instanceof AggregationDataType && id.contains(AUTO)) {
+			String[] idSections = StringUtils.split(id, AUTO);
+			writer.append(idSections[0])
+					.append(((AggregationDataType) dataType)
+							.getElementDataType().getId())
+					.append(idSections[1]);
+		} else {
+			writer.append(id);
+		}
+		writer.append('"');
+
+		if (useLazyDataType) {
 			writer.append(")");
 		} else {
-			if (useLazyDataType) {
-				writer.append("dorado.LazyLoadDataType.create(v.dataTypeRepository,");
-			}
-
-			writer.append('"');
-			String id = dataType.getId();
-			if (dataType instanceof AggregationDataType && id.contains(AUTO)) {
-				String[] idSections = StringUtils.split(id, AUTO);
-				writer.append(idSections[0])
-						.append(((AggregationDataType) dataType)
-								.getElementDataType().getId())
-						.append(idSections[1]);
-			} else {
-				writer.append(id);
-			}
-			writer.append('"');
-
-			if (useLazyDataType) {
-				writer.append(")");
-			} else {
-				if (context.isShouldOutputDataTypes()) {
-					if (dataType instanceof AggregationDataType) {
-						dataType = ((AggregationDataType) dataType)
-								.getElementDataType();
-					}
-					if (dataType != null) {
-						Map<String, DataType> includeDataTypes = context
-								.getIncludeDataTypes(true);
-						if (!includeDataTypes.containsKey(dataType.getName())) {
-							includeDataTypes.put(dataType.getName(), dataType);
-						}
+			if (context.isShouldOutputDataTypes()) {
+				if (dataType instanceof AggregationDataType) {
+					dataType = ((AggregationDataType) dataType)
+							.getElementDataType();
+				}
+				if (dataType != null) {
+					Map<String, DataType> includeDataTypes = context
+							.getIncludeDataTypes(true);
+					if (!includeDataTypes.containsKey(dataType.getName())) {
+						includeDataTypes.put(dataType.getName(), dataType);
 					}
 				}
 			}
 		}
+		// }
 		json.endValue();
 	}
 }
