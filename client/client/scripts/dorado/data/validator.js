9e4de153e74ee02028bc556362f473b67a3d3ec2
william.jiang <william.jiang@bstek.com>
2014-04-04 11:19:47 +0000

    再次修复#dorado7-5781

--- client/client/scripts/dorado/data/validator.js
+++ client/client/scripts/dorado/data/validator.js
@@ -472,37 +472,37 @@ dorado.validator.RangeValidator = $extend(dorado.validator.BaseValidator, /** @s
 	},
 
 	doValidate : function(data, arg) {
-		var invalid, message = '', subMessage = $resource("dorado.data.ErrorOrEqualTo"), data = ( typeof data == "number") ? data : parseFloat(data);
+		var invalidMin, invalidMax, message = '', subMessage = $resource("dorado.data.ErrorOrEqualTo"), data = ( typeof data == "number") ? data : parseFloat(data);
 		if(this._minValueValidateMode != "ignore") {
 			if(data == this._minValue && this._minValueValidateMode != "allowEquals") {
-				invalid = true;
+                invalidMin = true;
 			}
 			if(data < this._minValue) {
-				invalid = true;
+                invalidMin = true;
 			}
             if (this._minValueValidateMode == "notAllowEquals")  {
                 subMessage = '';
             }
-			if(invalid) {
+			if(invalidMin) {
 				message += $resource("dorado.data.ErrorNumberTooLess", subMessage, this._minValue);
 			}
 		}
 		if(this._maxValueValidateMode != "ignore") {
 			if(data == this._maxValue && this._maxValueValidateMode != "allowEquals") {
-				invalid = true;
+                invalidMax = true;
 			}
 			if(data > this._maxValue) {
-				invalid = true;
+                invalidMax = true;
 			}
             if (this._maxValueValidateMode == "notAllowEquals")  {
                 subMessage = '';
             }
-			if(invalid) {
+			if(invalidMax) {
 				if(message) message += '\n';
 				message += $resource("dorado.data.ErrorNumberTooGreat", subMessage, this._maxValue);
 			}
 		}
-		if(invalid) return message;
+		if(invalidMin || invalidMax) return message;
 	}
 });
 
