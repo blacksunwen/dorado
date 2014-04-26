d7f7be6a116cba65f325781bd2bfbe7583e17e52
benny.bao@bstek.com <benny.bao@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2014-04-26 04:03:13 +0000

    修正Tree节点在insert后立刻expand，此时无法设置currentNode的BUG

--- trunk/client/client/scripts/dorado/widget/tree/abstract-tree.js
+++ trunk/client/client/scripts/dorado/widget/tree/abstract-tree.js
@@ -76,15 +76,15 @@ dorado.widget.tree.TreeNodeRenderer = $extend(dorado.Renderer, {
 	 * 内部的渲染方法，供复写。
 	 */
 	doRender: function(cell, node, arg) {
-        /*
-		if (node._expanded && node._nodesData && node._nodesTimestamp !== undefined && node._nodesTimestamp !== node._nodesData.timestamp) {
+		// 判断子节点对应的EntityList是否已被Entity.reset
+		if (node._expanded && node._nodesData && !node._nodesData._observer) {
 			node.resetChildren();
 			node._expanded = false;
 			setTimeout(function() {
 				node.expandAsync();
 			}, 0);
 		}
-        */
+
 		var tree = node._tree, level = node.get("level"), hasChild = node.get("hasChild");	
 		var container = (cell.tagName.toLowerCase() == "div") ? cell : cell.firstChild;	
 		container.style.paddingLeft = ((level - 1) * tree._indent) + "px";
