62cd5de3bc62ef9ea16e888ff0f8d9c686ffe934
benny.bao@bstek.com <benny.bao@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2014-06-09 14:58:33 +0000

    修正UpdateAction的查看详情报错的BUG

--- trunk/client/client/scripts/dorado/widget/action/rpc-action-plus.js
+++ trunk/client/client/scripts/dorado/widget/action/rpc-action-plus.js
@@ -15,10 +15,10 @@
 	var exceptionDialogMinWidth = 300;
 	var exceptionDialogMaxWidth = 800;
 	var exceptionDialogMaxHeight = 500;
-	
+
 	dorado.widget.UpdateAction.alertException = function(e) {
 		currentException = e;
-		
+
 		$import("grid", function() {
 			var dialog = getExceptionDialog();
 			dialog.set({
@@ -32,24 +32,27 @@
 			dialog.show();
 		});
 	}
-	
+
 	function getExceptionDialog() {
 		if (!exceptionDialog) {
 			var doms = {};
 			var contentDom = $DomUtils.xCreate({
 				tagName: "DIV",
 				className: "d-exception-content",
-				content: [{
-					tagName: "SPAN",
-					className: "d-exception-icon",
-					contextKey: "iconDom"
-				}, {
-					tagName: "SPAN",
-					className: "d-exception-text",
-					contextKey: "textDom"
-				}]
+				content: [
+					{
+						tagName: "SPAN",
+						className: "d-exception-icon",
+						contextKey: "iconDom"
+					},
+					{
+						tagName: "SPAN",
+						className: "d-exception-text",
+						contextKey: "textDom"
+					}
+				]
 			}, null, doms);
-			
+
 			exceptionDialog = new dorado.widget.Dialog({
 				center: true,
 				modal: true,
@@ -59,63 +62,68 @@
 					$type: "Native"
 				},
 				buttonAlign: "right",
-				buttons: [{
-					caption: $resource("dorado.baseWidget.ExceptionDialogOK"),
-					width: 85,
-					onClick: function() {
-						exceptionDialog.hide();
+				buttons: [
+					{
+						caption: $resource("dorado.baseWidget.ExceptionDialogOK"),
+						width: 85,
+						onClick: function() {
+							exceptionDialog.hide();
+						}
 					}
-				}],
-                afterShow: function(dialog){
-                    var buttons = dialog._buttons, button;
-                    if(buttons){
-                        button = buttons[0];
-                        if(button && button._dom.style.display != "none") {
-                            button.setFocus();
-                        }
-                    }
-                },
+				],
+				afterShow: function(dialog) {
+					var buttons = dialog._buttons, button;
+					if (buttons) {
+						button = buttons[0];
+						if (button && button._dom.style.display != "none") {
+							button.setFocus();
+						}
+					}
+				},
 				beforeShow: function(dialog) {
 					getDetailLink().render(dialog._textDom);
-					
+
 					var dom = dialog._dom;
 					var $dom = jQuery(dom), $contentDom = jQuery(contentDom);
-					
+
 					var contentWidth = $fly(doms.iconDom).outerWidth() + $fly(doms.textDom).outerWidth();
 					if (contentWidth < exceptionDialogMinWidth) {
 						contentWidth = exceptionDialogMinWidth;
-					} else if (contentWidth > exceptionDialogMaxWidth) {
+					}
+					else if (contentWidth > exceptionDialogMaxWidth) {
 						contentWidth = exceptionDialogMaxWidth;
 					}
 					var dialogWidth = $dom.width(), panelWidth = $contentDom.width();
 					dialog._width = contentWidth + dialogWidth - panelWidth;
-					
+
 					var contentHeight = $contentDom.outerHeight();
 					if (contentHeight > exceptionDialogMaxHeight) {
 						contentHeight = exceptionDialogMaxHeight;
-					} else {
+					}
+					else {
 						contentHeight = null;
 					}
 					if (contentHeight) {
 						var dialogHeight = $dom.height(), panelHeight = $contentDom.height();
 						dialog._height = contentHeight + dialogHeight - panelHeight;
 					}
-					
+
 					dialog.doOnResize();
 				}
 			});
-			
+
 			var containerDom = exceptionDialog.get("containerDom");
 			containerDom.appendChild(contentDom);
-			
+
 			exceptionDialog._contentDom = contentDom;
 			exceptionDialog._iconDom = doms.iconDom;
 			exceptionDialog._textDom = doms.textDom;
 		}
 		return exceptionDialog;
 	}
-	
+
 	var detailLink, detailDialog;
+
 	function getDetailLink() {
 		if (!detailLink) {
 			detailLink = new dorado.widget.Link({
@@ -125,7 +133,7 @@
 					try {
 						showDetailExceptionDialog(currentException);
 					}
-					catch (e) {
+					catch(e) {
 						dorado.Exception.processException(e);
 					}
 				}
@@ -133,9 +141,9 @@
 		}
 		return detailLink;
 	}
-	
+
 	function showDetailExceptionDialog(e) {
-		
+
 		function translateMessage(items, messages) {
 			messages.each(function(message) {
 				var item = dorado.Object.clone(message);
@@ -146,46 +154,47 @@
 				items.push(item);
 			});
 		}
-				
+
 		var dialog = getDetailExceptionDialog();
 		var items = [], validateContext = e.validateContext;
-		
+
 		if (validateContext) {
 			if (validateContext.error) translateMessage(items, validateContext.error);
 			if (validateContext.warn) translateMessage(items, validateContext.warn);
 		}
-		
+
 		dialog._grid.set("items", items);
 		dialog.show();
 	}
-	
+
 	function getDetailExceptionDialog() {
 		if (!detailExceptionDialog) {
 			var grid = new dorado.widget.Grid({
 				readOnly: true,
 				dynaRowHeight: true,
-				columns: [{
-					property: "state",
-					caption: $resource("dorado.baseWidget.SubmitValidationDetailState"),
-					width: 36,
-					align: "center",
-					onRenderCell: function(self, arg) {
-						var iconClass;
-						switch (arg.data.state) {
-							case "error":{
-								iconClass = "d-update-action-icon-error";
-								break;
+				columns: [
+					{
+						property: "state",
+						caption: $resource("dorado.baseWidget.SubmitValidationDetailState"),
+						width: 36,
+						align: "center",
+						onRenderCell: function(self, arg) {
+							var iconClass;
+							switch(arg.data.state) {
+								case "error":
+								{
+									iconClass = "d-update-action-icon-error";
+									break;
+								}
+								case "warn":
+								{
+									iconClass = "d-update-action-icon-warn";
+									break;
+								}
 							}
-							case "warn":{
-								iconClass = "d-update-action-icon-warn";
-								break;
-							}
-						}
-						
-						var $dom = $fly(arg.dom);
-						$dom.empty();
-						if (offsetX) {
-							$dom.xCreate({
+
+							var $dom = $fly(arg.dom);
+							$dom.empty().xCreate({
 								tagName: "LABEL",
 								className: iconClass,
 								style: {
@@ -194,28 +203,30 @@
 									display: "inline-block"
 								}
 							});
-						}
-						arg.processDefault = false;
-					}
-				}, {
-					property: "text",
-					caption: $resource("dorado.baseWidget.SubmitValidationDetailMessage"),
-					wrappable: true
-				}, {
-					property: "property",
-					caption: $resource("dorado.baseWidget.SubmitValidationDetailProperty"),
-					width: 200,
-					resizeable: true,
-					onRenderCell: function(self, arg) {
-						if (arg.data.propertyCaption && arg.data.propertyCaption != arg.data.property) {
-							arg.dom.innerText = arg.data.propertyCaption + "(" + arg.data.property + ")";
 							arg.processDefault = false;
 						}
-						else {
-							arg.processDefault = true;
+					},
+					{
+						property: "text",
+						caption: $resource("dorado.baseWidget.SubmitValidationDetailMessage"),
+						wrappable: true
+					},
+					{
+						property: "property",
+						caption: $resource("dorado.baseWidget.SubmitValidationDetailProperty"),
+						width: 200,
+						resizeable: true,
+						onRenderCell: function(self, arg) {
+							if (arg.data.propertyCaption && arg.data.propertyCaption != arg.data.property) {
+								arg.dom.innerText = arg.data.propertyCaption + "(" + arg.data.property + ")";
+								arg.processDefault = false;
+							}
+							else {
+								arg.processDefault = true;
+							}
 						}
 					}
-				}],
+				],
 				onDataRowDoubleClick: function(self, arg) {
 					var currentItem = self.get("currentEntity");
 					if (currentItem && currentItem.entity) {
@@ -223,7 +234,7 @@
 					}
 				}
 			});
-			
+
 			detailExceptionDialog = new dorado.widget.Dialog({
 				caption: $resource("dorado.baseWidget.ShowSubmitValidationDetail"),
 				width: 800,
@@ -234,15 +245,18 @@
 				layout: {
 					regionPadding: 8
 				},
-				children: [{
-					$type: "ToolTip",
-					text: $resource("dorado.baseWidget.SubmitValidationDetailTip"),
-					floating: false,
-					arrowDirection: "bottom",
-					arrowAlign: "left"
-				}, grid]
+				children: [
+					{
+						$type: "ToolTip",
+						text: $resource("dorado.baseWidget.SubmitValidationDetailTip"),
+						floating: false,
+						arrowDirection: "bottom",
+						arrowAlign: "left"
+					},
+					grid
+				]
 			});
-			
+
 			detailExceptionDialog._grid = grid;
 		}
 		return detailExceptionDialog;
