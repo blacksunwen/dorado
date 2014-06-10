c6f96cb42f80eba0e5095597e82b1f5782f2a6fb
benny.bao@bstek.com <benny.bao@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2014-06-10 10:49:32 +0000

    #dorado7-5984 htmlcontainer 通过js设置content，无法触发滚动条

--- trunk/client/client/scripts/dorado/widget/html-container.js
+++ trunk/client/client/scripts/dorado/widget/html-container.js
@@ -19,13 +19,13 @@
 dorado.widget.HtmlContainer = $extend(dorado.widget.Container, /** @scope dorado.widget.HtmlContainer.prototype */ {
 	$className: "dorado.widget.HtmlContainer",
 	focusable: false,
-	
+
 	ATTRIBUTES: /** @scope dorado.widget.HtmlContainer.prototype */ {
-	
+
 		className: {
 			defaultValue: "d-html-container"
 		},
-		
+
 		/**
 		 * 子控件的内容。
 		 * <p>
@@ -46,21 +46,11 @@ dorado.widget.HtmlContainer = $extend(dorado.widget.Container, /** @scope dorado
 			setter: function(content) {
 				this._content = content;
 				if (this._ready && this._rendered) {
-					this._oldDom = this._dom;
-					this._dom = null;
-					
-					$fly(this._oldDom).empty();
-					this.replace(this._oldDom);
-					if (this._container && this._layout && this._layout._dom) {
-						var layoutDom = this._layout._dom;
-						this._container.appendChild(layoutDom);
-					}
-					
-					$fly(this._oldDom).remove();
+					this.applyContent(this._dom, content);
 				}
 			}
 		},
-		
+
 		/**
 		 * 作为子控件容器的子DOM对象的CSS选择表达式。
 		 * @type String
@@ -75,96 +65,67 @@ dorado.widget.HtmlContainer = $extend(dorado.widget.Container, /** @scope dorado
 			writeBeforeReady: true
 		}
 	},
-	
+
 	assignDom: function(dom) {
 		this._dom = dom;
 		if (dom) {
 			try {
 				dom.style.display = "";
 			}
-			catch (e) {
+			catch(e) {
 				// do nothing
 			}
 		}
 	},
-	
+
 	createDom: function() {
-		var dom;
-		var content = this._content;
-		if (content) {
-			if (content.constructor == String) {
-				var div = document.createElement("SPAN");
-				div.innerHTML = this._content;
-				dom = div; //(div.childNodes.length > 1) ? div : div.firstChild;
-			} else {
-				var doms = [];
-				this._xCreateContext = {};
-				this.pushHtmlElement(doms, this._content);
-				if (doms.length == 1) {
-					dom = doms[0];
-					if (dom.nodeType == 3) {
-						var div = document.createElement("SPAN");
-						div.appendChild(dom);
-						dom = div;
-					}
-				} else if (doms.length > 1) {
-					var div = document.createElement("SPAN");
-					for (var i = 0; i < doms.length; i++) {
-						div.appendChild(doms[i]);
-					}
-					dom = div;
-				}
-			}
-		}
-		if (!dom) dom = document.createElement("SPAN");
-		
-		var container = dom;
-		if (this._containerExpression) {
-			var jq = $fly(container).find(this._containerExpression);
-			if (jq && jq.length > 0) this._container = container = jq[0];
+		var dom = document.createElement("SPAN");
+		if (this._content) {
+			this.applyContent(dom, this._content);
 		}
 		return dom;
 	},
-	
+
+	applyContent: function(dom, content) {
+		var layoutDom;
+		if (this._layout && this._layout._dom) {
+			layoutDom = this._layout._dom;
+		}
+
+		this._xCreateContext = {};
+		if (content) {
+			if (content.constructor == String) {
+				dom.innerHTML = content;
+			}
+			else if (content.nodeType) {
+				dom.appendChild(content);
+			}
+			else {
+				$fly(dom).xCreate(content, null, this._xCreateContext);
+			}
+		}
+
+		var container = dom;
+		if (this._containerExpression) {
+			var jq = $fly(container).find(this._containerExpression);
+			if (jq && jq.length > 0) container = jq[0];
+		}
+		this._container = container;
+
+		if (container && layoutDom) {
+			container.appendChild(layoutDom);
+		}
+
+		if (this._ready && this._rendered) {
+			this.updateModernScroller();
+		}
+	},
+
 	refreshDom: function(dom) {
 		if (this.getRealHeight() || this._children.size) dom.style.display = "inline-block";
 		$invokeSuper.call(this, [dom]);
 	},
-	
-	pushHtmlElement: function(doms, content) {
-	
-		function doPush(doms, content, context) {
-			if (!content) return;
-			
-			if (content.constructor == String) {
-				var div = this._DIV_FOR_INNERHTML;
-				if (!div) this._DIV_FOR_INNERHTML = div = document.createElement("DIV");
-				div.innerHTML = content;
-				while (div.firstChild) {
-					var node = div.firstChild;
-					div.removeChild(node);
-					if (dorado.Browser.msie && node.nodeType == 3) { // IE may crash on div.appendChild(#text)
-						var span = document.createElement("SPAN");
-						span.appendChild(node);
-						node = span;
-					}
-					doms.push(node);
-				}
-			} else if (content.nodeType) {
-				doms.push(content);
-			} else {
-				doms.push($DomUtils.xCreate(content, null, context));
-			}
-		}
-		
-		if (content instanceof Array) {
-			for (var i = 0; i < content.length; i++) 
-				doPush(doms, content[i], this._xCreateContext);
-		} else {
-			doPush(doms, content, this._xCreateContext);
-		}
-	},
-	
+
 	/**
 	 * 根据contextKey返回一个内部的子DOM元素。
 	 * <p>此方法仅当content属性是通过Object或Object[]方式定义是有效，
