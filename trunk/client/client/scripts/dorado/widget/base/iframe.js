35d69bb5c80e2223ecdda7f6623062e64c7e12bb
frank.zhang@bstek.com <frank.zhang@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2014-06-10 11:59:22 +0000

    在IFrame不可见的时候，设置path属性后添加一个延时去实际修改url。

--- trunk/client/client/scripts/dorado/widget/base/iframe.js
+++ trunk/client/client/scripts/dorado/widget/base/iframe.js
@@ -28,14 +28,14 @@
 				defaultValue: "d-iframe"
 			},
 
-            /**
-             * <iframe>的name属性，用于部分需要设置该属性的场景，该属性需要在IFrame ready之前设置。
-             * @type String
-             * @attribute[writeBeforeReady]
-             */
-            name: {
-                writeBeforeReady: true
-            },
+			/**
+			 * <iframe>的name属性，用于部分需要设置该属性的场景，该属性需要在IFrame ready之前设置。
+			 * @type String
+			 * @attribute[writeBeforeReady]
+			 */
+			name: {
+				writeBeforeReady: true
+			},
 
 			/**
 			 * IFrame对应的路径。
@@ -48,18 +48,23 @@
 				setter: function(value) {
 					var frame = this, oldPath = frame._path, dom = frame._dom, doms = frame._doms;
 
-                    frame._path = value;
+					frame._path = value;
 
 					if (dom) {
 						$fly(doms.loadingCover).css("display", "block");
 
 						frame.releaseCurrentPage();
 
-                        $fly(doms.iframe).addClass("hidden");
-                        if (oldPath != value) {
-                            frame._loaded = false;
-                        }
-						frame.replaceUrl(value);
+						$fly(doms.iframe).addClass("hidden");
+						if (oldPath != value) {
+							frame._loaded = false;
+						}
+						if (frame.isActualVisible())
+							frame.replaceUrl(value);
+						else
+							setTimeout(function() {
+								frame.replaceUrl(value);
+							}, 10);
 					}
 				}
 			},
@@ -84,36 +89,36 @@
 			 * 当iframe中的页面加载完毕后触发。
 			 * 
 			 * @param {Object}
-			 *            self 事件的发起者，即组件本身。
+			 *			self 事件的发起者，即组件本身。
 			 * @param {Object}
-			 *            arg 事件参数。
+			 *			arg 事件参数。
 			 * @return {boolean} 是否要继续后续事件的触发操作，不提供返回值时系统将按照返回值为true进行处理。
 			 * @event
 			 */
 			onLoad: {}
 		},
 
-        getDomainInfo: function(domain) {
-            var regex = /^(http[s]?):\/\/([\w.]+)(:([\d]+))?/ig, result = regex.exec(domain);
-            if (result) {
-                return {
-                    protocol: result[1],
-                    domain: result[2],
-                    port: result[4]
-                };
-            } else {
-                return {};
-            }
-        },
+		getDomainInfo: function(domain) {
+			var regex = /^(http[s]?):\/\/([\w.]+)(:([\d]+))?/ig, result = regex.exec(domain);
+			if (result) {
+				return {
+					protocol: result[1],
+					domain: result[2],
+					port: result[4]
+				};
+			} else {
+				return {};
+			}
+		},
 
-        isSameDomain: function() {
-	        var iframeSrc = $url(this._path);
-            if (/^(http[s]?):/ig.test(iframeSrc)) {
-                var localDomain = this.getDomainInfo(location.href), frameDomain = this.getDomainInfo(iframeSrc);
-                return localDomain.protocol == frameDomain.protocol && localDomain.domain == frameDomain.domain && localDomain.port == frameDomain.port;
-            }
-            return true;
-        },
+		isSameDomain: function() {
+			var iframeSrc = $url(this._path);
+			if (/^(http[s]?):/ig.test(iframeSrc)) {
+				var localDomain = this.getDomainInfo(location.href), frameDomain = this.getDomainInfo(iframeSrc);
+				return localDomain.protocol == frameDomain.protocol && localDomain.domain == frameDomain.domain && localDomain.port == frameDomain.port;
+			}
+			return true;
+		},
 
 		releaseCurrentPage: function() {
 			var frame = this, doms = frame._doms;
@@ -155,7 +160,7 @@
 					tagName: "iframe",
 					className: "iframe hidden",
 					contextKey: "iframe",
-		            scrolling: dorado.Browser.isTouch ? "no" : "auto",
+					scrolling: dorado.Browser.isTouch ? "no" : "auto",
 					frameBorder: 0
 				}, {
 					tagName: "div",
@@ -176,65 +181,65 @@
 				} ]
 			}, null, doms);
 
-            if (frame._name != undefined)
-                doms.iframe.name = frame._name || "";
+			if (frame._name != undefined)
+				doms.iframe.name = frame._name || "";
 
 			frame._doms = doms;
 
 			return dom;
 		},
 
-        doOnAttachToDocument: function() {
-	        var frame = this, doms = frame._doms, iframe = doms.iframe;
-	        $fly(iframe).load(function() {
-		        $fly(doms.loadingCover).css("display", "none");
-		        // fix ie 6 bug....
-		        if (!(dorado.Browser.msie && dorado.Browser.version == 6)) {
-			        $fly(iframe).removeClass("hidden");
-		        }
+		doOnAttachToDocument: function() {
+			var frame = this, doms = frame._doms, iframe = doms.iframe;
+			$fly(iframe).load(function() {
+				$fly(doms.loadingCover).css("display", "none");
+				// fix ie 6 bug....
+				if (!(dorado.Browser.msie && dorado.Browser.version == 6)) {
+					$fly(iframe).removeClass("hidden");
+				}
 				if (!frame.isActualVisible()) {
 					frame._notifyResizeOnVisible = true;
 					frame.onActualVisibleChange();
 				}
-		        frame.fireEvent("onLoad", frame);
-		        if (frame.isSameDomain()) {
-			        if (frame._replacedUrl && frame._replacedUrl != BLANK_PATH) {
-				        frame._loaded = true;
-			        }
-		        } else if (iframe.src && iframe.src != BLANK_PATH) {
-			        frame._loaded = true;
-		        }
-	        });
-	        frame.doLoad();
-        },
+				frame.fireEvent("onLoad", frame);
+				if (frame.isSameDomain()) {
+					if (frame._replacedUrl && frame._replacedUrl != BLANK_PATH) {
+						frame._loaded = true;
+					}
+				} else if (iframe.src && iframe.src != BLANK_PATH) {
+					frame._loaded = true;
+				}
+			});
+			frame.doLoad();
+		},
 		
 		replaceUrl: function(url) {
-            var frame = this, doms = frame._doms, replacedUrl = $url(url || BLANK_PATH);
+			var frame = this, doms = frame._doms, replacedUrl = $url(url || BLANK_PATH);
 			delete frame._notifyResizeOnVisible;
-            if (frame.isSameDomain()) {
-	            frame._replacedUrl = replacedUrl;
-                if (frame.getIFrameWindow()) frame.getIFrameWindow().location.replace(replacedUrl);
-            } else {
-                $fly(doms.iframe).prop("src", replacedUrl);
-            }
+			if (frame.isSameDomain()) {
+				frame._replacedUrl = replacedUrl;
+				if (frame.getIFrameWindow()) frame.getIFrameWindow().location.replace(replacedUrl);
+			} else {
+				$fly(doms.iframe).prop("src", replacedUrl);
+			}
 		},
 
-        doLoad: function() {
-            var frame = this, doms = frame._doms;
-            $fly(doms.loadingCover).css("display", "");
+		doLoad: function() {
+			var frame = this, doms = frame._doms;
+			$fly(doms.loadingCover).css("display", "");
 			this.replaceUrl(frame._path);
-        },
+		},
 
-        reloadIfNotLoaded: function() {
-            var frame = this;
-            if (!frame._loaded && frame._path) {
-                frame.doLoad();
-            }
-        },
+		reloadIfNotLoaded: function() {
+			var frame = this;
+			if (!frame._loaded && frame._path) {
+				frame.doLoad();
+			}
+		},
 
-        cancelLoad: function() {
+		cancelLoad: function() {
 			this.replaceUrl(BLANK_PATH);
-        },
+		},
 
 		doOnResize: function() {
 			if (dorado.Browser.isTouch) {
@@ -245,15 +250,15 @@
 			}
 		},
 
-        /**
-         * 重新载入页面。
-         */
-        reload: function() {
-            var frame = this;
-	        frame.releaseCurrentPage();
-	        frame.replaceUrl(null);
-	        frame.replaceUrl(frame._path);
-        },
+		/**
+		 * 重新载入页面。
+		 */
+		reload: function() {
+			var frame = this;
+			frame.releaseCurrentPage();
+			frame.replaceUrl(null);
+			frame.replaceUrl(frame._path);
+		},
 		
 		onActualVisibleChange: function() {
 
