e9064aba07c42312d9f161c5c0ae0c1c93498c65
benny.bao <benny.bao@bstek.com>
2014-09-11 17:52:47 +0000

    微调皮肤

--- client/client/skins/modern/support.js
+++ client/client/skins/modern/support.js
@@ -2,4 +2,5 @@ $setting["widget.MessageBox.defaultModalType"] = "dark";
 $setting["widget.ListBox.defaultRowHeight"] = 26;
 $setting["widget.Grid.defaultRowHeight"] = 26;
 $setting["widget.Tree.defaultRowHeight"] = 26;
+$setting["widget.TextEditor.triggerWidth"] = 20;
 $setting["common.taskIndicator.daemon.type"] = "icon";
