512d506bdd0a97c57bf742c0212f1c5582580990
mark.li <mark.li@bstek.com>
2014-09-30 08:16:57 +0000

    为plugin添加listener功能：   - listener: com.bstek.dorado.vidorsupport.plugin.iapi.ICloudoListener

--- vidor-support/src/com/bstek/dorado/vidorsupport/internal/GlobalRuleSetFactory.java
+++ vidor-support/src/com/bstek/dorado/vidorsupport/internal/GlobalRuleSetFactory.java
@@ -1,5 +1,8 @@
 package com.bstek.dorado.vidorsupport.internal;
 
+import com.bstek.dorado.vidorsupport.plugin.iapi.ICloudoListener;
+import com.bstek.dorado.vidorsupport.plugin.iapi.PluginMeta;
+import com.bstek.dorado.vidorsupport.plugin.internal.PluginManager;
 import com.bstek.dorado.vidorsupport.rule.RuleSet;
 
 public class GlobalRuleSetFactory extends AbstractRuleSetFactory {
@@ -13,6 +16,14 @@ public class GlobalRuleSetFactory extends AbstractRuleSetFactory {
 
 	@Override
 	protected void doCapture(RuleSet rs) {
+		PluginMeta[] metas = PluginManager.getInstance().getPlugins();
+		for (PluginMeta meta: metas) {
+			ICloudoListener l = meta.getCloudoListener();
+			if (l != null) {
+				l.onInitRuleSet(rs);
+			}
+		}
+		
 		this.singleRS = rs;
 	}
 
