2952f30bfeacafe12f1af906fe31184cd28a8a7f
william.jiang <william.jiang@bstek.com>
2014-12-11 05:09:58 +0000

    临时修复DataGrid在filter情况下,数据提交后被删除的数据还显示在DataGrid中的BUG

--- client/client/scripts/dorado/widget/grid/data-grid.js
+++ client/client/scripts/dorado/widget/grid/data-grid.js
@@ -604,18 +604,11 @@
 				case dorado.widget.DataSet.MESSAGE_REFRESH:{
 					this.hideCellEditor();
 					if (this._itemModel.groups) this._itemModel.refreshItems();
-//					if (this._itemModel.filtered && this._itemModel._items instanceof Array) {
-//						var entityList = this.getBindingData({
-//							firstResultOnly: true,
-//							acceptAggregation: true
-//						});
-//						this._itemModel.setItems(entityList);	
-//						if (this._filterMode == "clientSide") {
-//							this.filter();
-//						}
-//					} else {
+					if (this._itemModel.filtered && this._filterMode == "clientSide" && this._itemModel._items instanceof Array) {
+						this.filter();
+					} else {
 						this.refresh(true);
-//					}
+					}
 					break;
 				}
 				case dorado.widget.DataSet.MESSAGE_CURRENT_CHANGED:{
