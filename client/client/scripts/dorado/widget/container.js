64bdd10bec49b23d60a1d1e0d75767f20adacc59
frank.zhang <frank.zhang@bstek.com>
2015-03-13 07:32:47 +0000

    解决移动设备下Container设置contentOverflowX、contentOverflowY无效的问题。

--- client/client/scripts/dorado/widget/container.js
+++ client/client/scripts/dorado/widget/container.js
@@ -539,8 +539,16 @@
 					(overflowX == "auto" || overflowY == "auto" || overflowX == "scroll" || overflowY == "scroll")) {
 					contentCt.style.overflowX = overflowX;
 					contentCt.style.overflowY = overflowY;
+					var direction = null;
+					if (overflowX = "hidden") {
+						direction = "vertical";
+					}
+					if (overflowY == "hidden") {
+						direction = "horizontal";
+					}
 					container._modernScroller = $DomUtils.modernScroll(contentCt, {
-						autoDisable: true
+						autoDisable: true,
+						direction: direction
 					});
 					$fly(contentCt).bind("modernScrolling", function() {
 						dorado.Toolkits.setDelayedAction(container, "$onScrollTimerId", function() {
