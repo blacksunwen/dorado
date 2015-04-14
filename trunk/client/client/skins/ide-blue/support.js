2e3decc450c4847fc0ee8f9eb63261ed9e0fb0f2
benny.bao@bstek.com <benny.bao@bstek.com@0189df41-6526-4f5f-895b-eb3a729c4730>
2015-04-14 09:20:28 +0000

    新增ide-blue皮肤

---
+++ trunk/client/client/skins/ide-blue/support.js
@@ -0,0 +1,6 @@
+$setting["widget.MessageBox.defaultModalType"] = "dark";
+$setting["widget.ListBox.defaultRowHeight"] = 22;
+$setting["widget.Grid.defaultRowHeight"] = 22;
+$setting["widget.Tree.defaultRowHeight"] = 25;
+$setting["widget.TextEditor.triggerWidth"] = 24;
+$setting["common.taskIndicator.daemon.type"] = "icon";
